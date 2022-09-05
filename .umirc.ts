5f6828b63c372d02d3e2dba45ac5df0705771b0c
huiergo <1063229610@qq.com>
2022-09-05 23:32:06 +0800

    feat: update

--- .umirc.ts
+++ .umirc.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   logo: 'https://gw.alipayobjects.com/zos/bmw-prod/d60657df-0822-4631-9d7c-e7a869c2f21c/k79dmz3q_w126_h126.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  base: '/heima-ui-3/',
-  publicPath: '/heima-ui-3/',
+  base: '/heima-ui-fuben/',
+  publicPath: '/heima-ui-fuben/',
   exportStatic: {},
   // more config: https://d.umijs.org/config
 });
